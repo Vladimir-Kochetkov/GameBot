683bc5e948d7d91cb036045a66c6afedff49aba1
Vladimir-Kochetkov <js.kochetkov@gmail.com>
2022-02-03 21:53:49 +0300

    add(script.js):solution 6 tasks

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict";
+function global() {
+    let number = random();
+    alert('Угадай число от 1 до 100');
+    let guess = prompt('Какое число я загадал?');
+    console.log(guess);
+    let isNumber = function (n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+
+    function random() {
+        return Math.ceil(Math.random() * 100);
+    }
+
+    function random1() {
+        if (guess === null) {
+            guess = alert('Игра окончена');
+            global();
+
+        } else if (guess < number) {
+            guess = +prompt('Загадайте число больше');
+            random1();
+        } else if (!isNumber(guess)) {
+            guess = +prompt('Введите число');
+            random1();
+        } else if (guess > number) {
+            guess = +prompt('Загадайте число меньше');
+            random1();
+
+        } else if (guess === number) {
+            alert('Поздравляю, Вы угадали');
+            global();
+        }
+    }
+    random1();
+}
+global();
