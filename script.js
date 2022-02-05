dd9c48d98ab00d7009bbb73389713b525e55de77
Vladimir-Kochetkov <js.kochetkov@gmail.com>
2022-02-06 00:08:50 +0300

    add(script.js):complicated 6 task

--- script.js
+++ script.js
@@ -11,23 +11,36 @@ function global() {
     function random() {
         return Math.ceil(Math.random() * 100);
     }
-
+    let attempt = 10;
     function random1() {
+        attempt--;
+        if (attempt < 1) {
+            let b = confirm('Попытки закончились, хотите сыграть еще?');
+            if (b === true) {
+                global();
+            } else {
+                return;
+            }
+        }
         if (guess === null) {
             guess = alert('Игра окончена');
         } else if (guess < number) {
-            guess = +prompt('Загадайте число больше');
+            guess = +prompt(`Загаданное число больше, осталось попыток ${attempt}`);
             random1();
         } else if (!isNumber(guess)) {
             guess = +prompt('Введите число');
+            attempt++;
             random1();
         } else if (guess > number) {
-            guess = +prompt('Загадайте число меньше');
+            guess = +prompt(`Загаданное число меньше, осталось попыток ${attempt}`);
             random1();
-
         } else if (guess === number) {
-            alert('Поздравляю, Вы угадали');
-            global();
+            let a = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+            if (a === true) {
+                global();
+            } else if (a === false) {
+                return;
+            }
         }
     }
     random1();
